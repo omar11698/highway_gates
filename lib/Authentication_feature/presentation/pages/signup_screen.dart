aba88f213c462f871328cad7da58305ebcfd1586
omar1161998 <almatador.omaga@gmail.com>
2023-05-07 09:16:19 +0300

    over flow Changes

--- lib/Authentication_feature/presentation/pages/signup_screen.dart
+++ lib/Authentication_feature/presentation/pages/signup_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:highway_gates/Authentication_feature/presentation/widgets/default_button.dart';
 import 'package:highway_gates/Authentication_feature/presentation/widgets/login_textformfield.dart';
@@ -21,51 +20,53 @@ class SignUpScreen extends StatelessWidget {
     TextEditingController passwordsTextEditingController=TextEditingController();
     TextEditingController verifyPasswordEditingController=TextEditingController();
 
-    return Scaffold(
-        appBar: AppBar(
+    return SafeArea(
+      child: Scaffold(
+          appBar: AppBar(
+            backgroundColor: Colors.white,
+            leading: InkWell(
+              onTap: ()=>Navigator.of(context).pop(),
+              child: const Icon(
+                Icons.arrow_back_ios,
+                size: 25,
+                color: Colors.black,
+              ),
+            ),
+            elevation: 0,
+          ),
           backgroundColor: Colors.white,
-          leading: InkWell(
-            onTap: ()=>Navigator.of(context).pop(),
-            child: const Icon(
-              Icons.arrow_back_ios,
-              size: 25,
-              color: Colors.black,
-            ),
-          ),
-          elevation: 0,
-        ),
-        backgroundColor: Colors.white,
-        body: Padding(
-          padding: const EdgeInsets.all(20.0),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.start,
-              children: [
-                const Text(strCreateNewAcc,style: TextStyle(fontWeight: FontWeight.w700,fontSize: 20),),
+          body: Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: SingleChildScrollView(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: [
+                  const Text(strCreateNewAcc,style: TextStyle(fontWeight: FontWeight.w700,fontSize: 20),),
+                  spaceBetweenWidgets,
+                  const Align(alignment: Alignment.center,child: Text(strSetupYourAccount,style: TextStyle(fontWeight: FontWeight.w500,fontSize: 16,color: Colors.grey),)),
+
+               /// content dialog
+               content(nameTextEditingController, spaceBetweenWidgets, emailTextEditingController, passwordsTextEditingController, verifyPasswordEditingController),
                 spaceBetweenWidgets,
-                const Align(alignment: Alignment.center,child: Text(strSetupYourAccount,style: TextStyle(fontWeight: FontWeight.w500,fontSize: 16,color: Colors.grey),)),
+                spaceBetweenWidgets,
 
-             /// content dialog
-             content(nameTextEditingController, spaceBetweenWidgets, emailTextEditingController, passwordsTextEditingController, verifyPasswordEditingController),
-              spaceBetweenWidgets,
-              spaceBetweenWidgets,
+                /// sign up button
+                DefaultButton(mobileSize: mobileSize, label: "Sign Up"),
+                spaceBetweenWidgets,
 
-              /// sign up button
-              DefaultButton(mobileSize: mobileSize, label: "Sign Up"),
-              spaceBetweenWidgets,
-
-              alreadyHaveAccount(mobileSize, context),
-              spaceBetweenWidgets,
-              spaceBetweenWidgets,
-              orRow(),
-             spaceBetweenWidgets,
-             googleAndFacebookAuth(mobileSize),
+                alreadyHaveAccount(mobileSize, context),
+                spaceBetweenWidgets,
+                spaceBetweenWidgets,
+                orRow(),
+               spaceBetweenWidgets,
+               googleAndFacebookAuth(mobileSize),
 
 
-              ],
+                ],
+              ),
             ),
-          ),
-        ));
+          )),
+    );
   }
 
   Padding content(TextEditingController nameTextEditingController, SizedBox spaceBetweenWidgets, TextEditingController emailTextEditingController, TextEditingController passwordsTextEditingController, TextEditingController verifyPasswordEditingController) {
--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:highway_gates/Core/constants/text_styles.dart';
 import 'Core/router/navigation_router.dart';
