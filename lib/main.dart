0a0bd7de11d5820700ae8f66251b76f7ac807c37
omar1161998 <almatador.omaga@gmail.com>
2023-05-09 07:45:20 +0300

    firebase options and configuration added

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:highway_gates/Core/constants/text_styles.dart';
+import 'package:highway_gates/firebase_options.dart';
 import 'Core/router/navigation_router.dart';
 import 'package:firebase_core/firebase_core.dart';
 
-void main() async {
-  // await Firebase.initializeApp();
+Future<void> main() async {
+
+  /// fire base binding
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(const MyApp());
+
+
+
 }
 
 class MyApp extends StatelessWidget {
